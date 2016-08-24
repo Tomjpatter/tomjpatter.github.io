34a67dce91d5adb92e61fc0e00c233c1bb8c5d7e
Tom P <tjpatterson98@yahoo.com>
2016-08-24 08:24:19 +0000

    Cinema set up I

---
+++ GT/gtMain.js
@@ -0,0 +1,17 @@
+/*
+	Author: 	Tom Patterson
+	Created: 	8/24/16
+	Revised: 	N/A
+	Title: 		Movie player
+	Purpose: 	Play Youtube videos
+	Notes:
+*/
+
+var baseAddress ="https://www.youtube.com/embed/";
+var movie = document.getElementById("movie");
+var player = document.getElementById("player");
+var titles = document.getElementById("titles");
+
+var titlesArray = ["p5ysrH-L6t8","lBLG_O5zm9Y","oilJoJ48Vns","NuQRdyVWdDw","csU50fjGPHw"];
+
+ 

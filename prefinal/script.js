cf17a5b7b13df866554b7c9cf2326226dd16252a
Tom P <tjpatterson98@yahoo.com>
2016-09-15 18:14:39 +0000

    new

--- prefinal/script.js
+++ prefinal/script.js
@@ -1,8 +1,10 @@
 //alert("testing");
 var btn = document.getElementById("btn");
 var ccUrl="https://www.codecademy.com/TomP."
+var btn2 = document.getElementById("btn2");
 
 btn.onmousedown = showAcademy;
+btn2.onmousedown = newTabAcademy()
 
 
 function showAcademy(){
@@ -11,5 +13,5 @@ function showAcademy(){
 }
 
 function newTabAcademy(){
-    window.open(ccUrl);
+   window.open(ccUrl);
 }
